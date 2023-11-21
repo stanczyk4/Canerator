c8683fd0a23e1d93ff92601ae5b8fc9f88751caf
Michael Stanczyk <stanczyk4@gmail.com>
2023-11-20 20:33:57 -0600

    override all

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,29 @@ namespace app
         {
         }
         virtual ~MainApp() {}
+
+        // Update tick
         void OnUpdate() override { app.Update(); }
+
+        // Input events
+        void OnKeyEvent([[maybe_unused]] int key,
+                        [[maybe_unused]] int scancode,
+                        [[maybe_unused]] int action,
+                        [[maybe_unused]] int mods) override
+        {
+        }
+        void OnKeyPress([[maybe_unused]] int key, [[maybe_unused]] int mods) override {}
+        void OnMouseButtonEvent([[maybe_unused]] int button,
+                                [[maybe_unused]] int action,
+                                [[maybe_unused]] int mods) override
+        {
+        }
+        void OnCursorPosition([[maybe_unused]] double x, [[maybe_unused]] double y) override {}
+
+        // Window messages
+        void OnWindowPositionChange([[maybe_unused]] int x, [[maybe_unused]] int y) override {}
+        void OnWindowSizeChange([[maybe_unused]] int width, [[maybe_unused]] int height) override {}
+        void OnWindowMaximize([[maybe_unused]] bool maximized) override {}
 
       private:
         Canerator app;
